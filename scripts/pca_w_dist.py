1cd6c47438ce424e74b6402aa9465135b5ae0fea
Francesco Randi <francesco.randi@gmail.com>
2022-09-21 09:12:14 -0400

    updated power spectrum

--- scripts/pca_w_dist.py
+++ scripts/pca_w_dist.py
@@ -1,14 +1,18 @@
 import numpy as np, matplotlib.pyplot as plt, re, sys
 from sklearn.decomposition import PCA
+from scipy.stats import kstest
 import wormdatamodel as wormdm
 
 ds_list_file = "/projects/LEIFER/francesco/spontdyn_list.txt"
 tagss = ["488 AML32","488 AML70","505 AML32","505 AML70","488 AKS521.1.i","488 AKS522.1.i","505+405simul AML32",]
-cs = ["C0","C0","C1","C1","C2","C3","C0"]
-tagss = ["488 AML32","488 AML70","488 AKS521.1.i","488 AKS522.1.i",]
-cs = ["C0","C0","C2","C3"]
-tagss = ["488 AML32","488 AML70"]#,#["505 AML32","505 AML70"]#["488 AML32","488 AML70"]#,
-cs = ["C"+str(i) for i in np.arange(len(tagss))]
+group = [0,0,1,1,0,0,0]
+cs = ["C"+str(g) for g in group]
+
+#cs = ["C"+str(i) for i in np.arange(len(tagss))]
+
+#tagss = ["488 AML32","488 AML70","505+405simul AML32"]
+#group = [0,0,1,1]
+#cs = ["C"+str(g) for g in group]
 
 signal_kwargs = {"remove_spikes": True,  "smooth": True, 
                  "nan_interp": True, 
@@ -20,10 +24,9 @@ plot = "--plot" in sys.argv
 average = "--average" in sys.argv
 
 # Define spectral ranges in which to look for oscillations
-T_range = np.array([300.,30.])
+T_range = np.array([300.,50.])
 f_range = 1./T_range
 print("spectral range",np.around(f_range,3))
-
 
 fig4 = plt.figure(4)
 ax4 = fig4.add_subplot(111)
@@ -31,9 +34,15 @@ ax4 = fig4.add_subplot(111)
 fig5 = plt.figure(5)
 ax5 = fig5.add_subplot(111)
 
+fig7 = plt.figure(7)
+ax7 = fig7.add_subplot(111)
+
 if average:
     fig6 = plt.figure(6)
     ax6 = fig6.add_subplot(111)
+    
+maxfss = np.empty(len(np.unique(group)),dtype=object)
+for i in np.arange(len(maxfss)): maxfss[i] = []
 
 for k in np.arange(len(tagss)):
     tags = tagss[k]
@@ -47,15 +56,23 @@ for k in np.arange(len(tagss)):
     stdws = []
     maxfs = []
     ps = []
+    n_neurons = []
     for i in np.arange(n):
         folder = ds_list[i]
         rec = wormdm.data.recording(folder,legacy=True,rectype="3d",settings={"zUmOverV":200./10.})
         sig = wormdm.signal.Signal.from_file(folder,"gRaw",**signal_kwargs)
+        n_neurons.append(sig.data.shape[1])
         
         pca = PCA()
         pcs = pca.fit_transform(sig.data)
         weights = pca.components_
         expl_var = pca.explained_variance_ratio_
+        
+        if plot:
+            fig11 = plt.figure(11)
+            ax = fig11.add_subplot(111)
+            ax.plot(expl_var,'o')
+            ax.set_title(tags)
         
         ftpc = np.fft.fft(pcs,axis=0,norm="ortho")
         f = np.fft.fftfreq(pcs.shape[0],d=rec.Dt)
@@ -101,7 +118,7 @@ for k in np.arange(len(tagss)):
                 ax = fig2.add_subplot(3,3,j+1)
                 ax.bar(np.arange(len(weights_sorted)),weights_sorted)
                 ax.set_title(str(np.around(stdw,2)))
-            
+                            
         if plot:
             ax3 = fig3.add_subplot(111)
             ax3.imshow(sig.data.T,aspect="auto")
@@ -126,10 +143,22 @@ for k in np.arange(len(tagss)):
     # Make histogram of frequencies. If average, plot both first and second PC
     # separately.
     if not average:
-        ax5.hist(maxfs,label=tags,color=cs[k],alpha=0.2)
+        for j in np.arange(n):
+            # INSTEAD OF APPENDING JUST ONE MAXFS, YOU NEED TO APPEND ONE MAXFS 
+            # FOR EACH NEURON PARTICIPATING IN THE PC (STDWS*TOT_N_NEURONS)
+            for jj in np.arange(int(stdws[j]*n_neurons[j])):
+                maxfss[group[k]].append(maxfs[j])
+        #maxfss[group[k]] = maxfss[group[k]]+maxfs.tolist()
+        ax5.hist(maxfs,label=tags,color=cs[k],alpha=0.2,density=True)
     else:
-        ax5.hist(maxfs[::2],label=tags,color=cs[k],alpha=0.3)
-        ax6.hist(maxfs[1::2],label=tags,color=cs[k],alpha=0.2)
+        for j in np.arange(n):
+            # INSTEAD OF APPENDING JUST ONE MAXFS, YOU NEED TO APPEND ONE MAXFS 
+            # FOR EACH NEURON PARTICIPATING IN THE PC (STDWS*TOT_N_NEURONS)
+            for jj in np.arange(int(stdws[j]*n_neurons[j])):
+                maxfss[group[k]].append(maxfs[j])
+        #maxfss[group[k]] = maxfss[group[k]]+maxfs[::2].tolist()
+        ax5.hist(maxfs[::2],label=tags,color=cs[k],alpha=0.3,density=True)
+        ax6.hist(maxfs[1::2],label=tags,color=cs[k],alpha=0.2,density=True)
     
     if average: 
         maxfs = (maxfs[::2]*ps[::2]+maxfs[1::2]*ps[1::2])/(ps[::2]+ps[1::2])
@@ -146,7 +175,29 @@ for k in np.arange(len(tagss)):
     ax4.scatter(x,y,s=markersize,label=tags,color=cs[k])
     
     
+# Print p-value
+alt ="less"#"two-sided"
+_,pval = kstest(maxfss[0],maxfss[1],alternative=alt)
+print("p ("+alt+")",pval)
 
+# Plot histogram compiled by groups
+for g in np.unique(group):
+    lbl = ""
+    for it in np.arange(len(tagss)):
+        if group[it] == g:
+            tags = tagss[it]
+            tags = re.sub("AML32","wt",tags)
+            tags = re.sub("AML70","lite-1",tags)
+            tags = re.sub("AKS521.1.i","gur-3",tags)
+            tags = re.sub("AKS522.1.i","lite-1;gur-3",tags)
+            lbl += tags+","
+    ax7.hist(maxfss[g],bins=30,range=f_range,label=lbl,color="C"+str(g),alpha=0.2,)
+ax7.set_xlabel("frequency (Hz)")
+ax7.set_ylabel("number")
+pvals = str(pval).split("e")[0][:3]+"e"+str(pval).split("e")[1]
+ax7.set_title("p ("+alt+") "+pvals)
+ax7.legend()
+fig7.tight_layout()
 
 ax4.set_xlabel("explained variance")
 ax4.set_ylabel("peak frequency (Hz)")
@@ -157,13 +208,13 @@ ax4.legend()
 fig4.tight_layout()
 
 ax5.set_xlabel("frequency (Hz)")
-ax5.set_ylabel("number")
+ax5.set_ylabel("density")
 ax5.legend()
 fig5.tight_layout()
 
 if average:
     ax6.set_xlabel("frequency (Hz)")
-    ax6.set_ylabel("number")
+    ax6.set_ylabel("density")
     ax6.legend()
     fig6.tight_layout()
 plt.show()
--- scripts/raw_spectrum.py
+++ scripts/raw_spectrum.py
@@ -1,20 +1,22 @@
 import numpy as np, matplotlib.pyplot as plt, re, sys
 from sklearn.decomposition import PCA
+from scipy.stats import kstest
 import wormdatamodel as wormdm
 
 ds_list_file = "/projects/LEIFER/francesco/spontdyn_list.txt"
-tagss = ["488 AML32","488 AML70","505 AML32","505 AML70","488 AKS521.1.i","488 AKS522.1.i","505+405simul AML32",]
-cs = ["C0","C0","C1","C1","C2","C3","C0"]
+tagss = ["488 AML32","505 AML32","488 AML70","488 AKS521.1.i","488 AKS522.1.i",]
+group = [0,1,0,1,1,]
+#cs = ["C"+str(g) for g in group]
 cs = ["C"+str(i) for i in np.arange(len(tagss))]
 
-signal_kwargs = {"remove_spikes": True,  "smooth": True, 
+signal_kwargs = {"remove_spikes": True,  "smooth": False, 
                  "nan_interp": True, 
                  "smooth_mode": "sg_causal", 
                  "smooth_n": 13, "smooth_poly": 1,
-                 "photobl_appl":True}
+                 "photobl_appl": True}
 
 # Define spectral ranges in which to look for oscillations
-T_range = np.array([300.,30.])
+T_range = np.array([100.,30.])
 f_range = 1./T_range
 f = np.linspace(f_range[0],f_range[1],100)
 print("spectral range",np.around(f_range,3))
@@ -22,8 +24,27 @@ print("spectral range",np.around(f_range,3))
 fig1 = plt.figure(1)
 ax1 = fig1.add_subplot(111)
 
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(111)
+
+fig3 = plt.figure(3)
+ax3 = fig3.add_subplot(111)
+
+fig4 = plt.figure(4)
+ax4 = fig4.add_subplot(111)
+
+fig5 = plt.figure(5)
+ax5 = fig5.add_subplot(111)
+
+maxfss = np.empty(len(np.unique(group)),dtype=object)
+for i in np.arange(len(maxfss)): maxfss[i] = []
+
+fracps = np.empty(len(tagss),dtype=object)
+for i in np.arange(len(fracps)): fracps[i] = np.empty(0)
+
 for k in np.arange(len(tagss)):
     tags = tagss[k]
+    g = group[k]
     ds_list = wormdm.signal.file.load_ds_list(ds_list_file,tags=tags,exclude_tags=None)
     n = len(ds_list)
     
@@ -40,28 +61,96 @@ for k in np.arange(len(tagss)):
         rec = wormdm.data.recording(folder,legacy=True,rectype="3d",settings={"zUmOverV":200./10.})
         sig = wormdm.signal.Signal.from_file(folder,"gRaw",**signal_kwargs)
         
-        ftsig = np.fft.fft(sig.data,axis=0,norm="ortho")
+        ftsig = np.fft.fft(sig.data-np.average(sig.data,axis=0),axis=0,norm="ortho")
         ftsig = np.absolute(ftsig)**2
-        f_ = np.fft.fftfreq(sig.data.shape[0],d=rec.Dt)
-        
-        totp = np.sum(ftsig)
         
         # Slice frequency range specified above
-        f0 = np.argmin(np.abs(f-f_range[0]))
-        f1 = np.argmin(np.abs(f-f_range[1]))
+        f_ = np.fft.fftfreq(sig.data.shape[0],d=rec.Dt)
+        f0 = np.argmin(np.abs(f_-f_range[0]))
+        f1 = np.argmin(np.abs(f_-f_range[1]))
         
-        avgft_ = np.average(ftsig,axis=1)/totp
+        # Calculate fraction of power inside frequency range
+        totp = np.sum(ftsig,axis=0)
+        fracp = np.sum(ftsig[f0:f1]/totp,axis=0)
+        #fracps[k].append(fracp)
+        fracps[k] = np.append(fracps[k],fracp)
+        
+        # Store peak frequency 
+        for m in np.arange(sig.data.shape[1]):
+            maxf = f[f0+np.argmax(np.absolute(ftsig[f0:f1,m]))]
+            maxfss[g].append(maxf)
+        
+        avgft_ = np.average(ftsig/totp,axis=1)
         avgft = np.interp(f,f_[f0:f1],avgft_[f0:f1])
         avgfts.append(avgft)
         
     avgfts = np.array(avgfts)
     avgavgft = np.average(avgfts,axis=0)
     
-    ax1.plot(f,avgavgft,label=tags,c=cs[k])
-    
+    ax1.plot(f,np.convolve(avgavgft,np.ones(1),mode="same"),label=tags,c=cs[k])
+
 ax1.set_xlabel("frequency (Hz)")
 ax1.set_ylabel("average power spectrum")
 ax1.legend()
 fig1.tight_layout()
+
+for g in np.unique(group):
+    ax2.hist(maxfss[g],label="G"+str(g),color="C"+str(g),alpha=0.2,density=True)
+ax2.set_xlabel("frequency (Hz)")
+ax2.set_ylabel("density")
+ax2.legend()
+fig2.tight_layout()
+
+dn = 1.0
+d = 0.2
+bar_width = 0.4
+dy = 0.05
+lbls = []
+for k in np.arange(len(tagss)):
+    tags = tagss[k]
+    tags = re.sub("AML32","wt",tags)
+    tags = re.sub("AML70","lite-1",tags)
+    tags = re.sub("AKS521.1.i","gur-3",tags)
+    tags = re.sub("AKS522.1.i","lite-1;gur-3",tags)
+    
+    stars = ""
+    if k>0:
+        _,pval = kstest(np.ravel(fracps[0]),np.ravel(fracps[k]),alternative="less")
+        print(pval)
+        if pval<0.05: stars = "*"
+        if pval<0.01: stars = "**"
+        if pval<0.001: stars = "***"
+        
+    y = fracps[k]
+    yerr = np.std(fracps[k])
+    
+    ax3.bar(k*dn,np.average(y),color=cs[k],width=bar_width,alpha=0.6,label=tags)
+    ax3.scatter(k*dn+np.random.random(len(fracps[k]))*bar_width/2 - bar_width/4, fracps[k], color=cs[k],s=0.5)
+    ax3.text(k, 0.35, stars)
+    
+    #ax5.violinplot(y,positions=[k],showmeans=False,showextrema=False,quantiles=None)
+    ax5.boxplot(y,positions=[k],boxprops={"color":cs[k],"linewidth":2},medianprops={"color":cs[k],"linewidth":2},widths=0.8)
+    ax5.text(k, -0.03, stars, ha="center")
+    
+    ax4.hist(fracps[k],label=tags,alpha=0.2)
+    lbls.append(tags)
+
+ax3.set_ylabel("fraction of power in "+str(np.around(f_range,3))+" Hz")
+ax3.set_xticks(np.arange(len(tagss)))
+ax3.set_xticklabels(lbls)
+fig3.tight_layout()
+
+ax4.set_xlabel("fraction of power in "+str(np.around(f_range,3))+" Hz")
+ax4.set_ylabel("density")
+ax4.legend()
+fig4.tight_layout()
+
+ax5.set_ylim(-0.04,None)
+ax5.set_xticks(np.arange(len(tagss)))
+ax5.set_xticklabels(lbls)
+ax5.set_yticks([0,0.1,0.2,0.3])
+ax5.set_ylabel("fraction of power in "+str(np.around(f_range,3))+" Hz")
+fig5.tight_layout()
+fig5.savefig("/projects/LEIFER/francesco/spontdyn/power_spectrum_bars.png",dpi=300,bbox_inches="tight")
 
 plt.show()
